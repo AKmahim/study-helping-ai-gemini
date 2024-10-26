e9a5600b841913ba50eada9f52fd30f670746df5
AKmahim <mdmahimhossen134@gmail.com>
2024-10-26 09:29:55 +0600

    chatbot gemini

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from openai.embeddings_utils import EmbeddingsUtils
-from openai.api_base import APIBase
-from openai.error import OpenAIError
-import openai
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
@@ -30,13 +26,13 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
 
 def get_vector_store(text_chunks):
-    embeddings = EmbeddingsUtils.create_embedding_model()
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
@@ -44,22 +40,28 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question in Bengali as accurately as possible. If the answer is not available in the context, say "answer is not available in the context". Do not provide wrong answers.\n\n
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
+
     Answer:
     """
 
-    model = APIBase.create_model("mpt-7b")  # or "mpt-13b"
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt_template)
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
     return chain
 
 
 
 def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", allow_dangerous_deserialization=True)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
@@ -82,10 +84,7 @@ def main():
     user_question = st.text_input("Ask a Question from the PDF Files")
 
     if user_question:
-        try:
-            user_input(user_question)
-        except OpenAIError as e:
-            st.error(f"An error occurred: {e}")
+        user_input(user_question)
 
     with st.sidebar:
         st.title("Menu:")
