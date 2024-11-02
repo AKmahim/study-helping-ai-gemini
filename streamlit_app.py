50e5a8806a1fdeced6db2704181978b3a081e572
AKmahim <mdmahimhossen134@gmail.com>
2024-11-03 00:27:29 +0600

    chagne api

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+os.getenv("MISTRAL_API_KEY")
+genai.configure(api_key=os.getenv("MISTRAL_API_KEY"))
 
 
 def get_pdf_text(pdf_docs):
